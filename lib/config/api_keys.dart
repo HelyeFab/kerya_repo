26768de0be9c3d82690c3d3e0843e29d04aabb96
Emmanuel Fabiani <63291559+HelyeFab@users.noreply.github.com>
2025-01-02 17:30:03 +0000

    chore: remove sensitive files and update gitignore

--- lib/config/api_keys.dart
+++
@@ -1,7 +0,0 @@
-// TODO: Move to secure storage in production
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class ApiKeys {
-  static String? get googleApiKey => dotenv.env['GOOGLE_TRANSLATE_API_KEY'];
-  static const String gooLabsApiKey = '993108e7c1e48c6056be45cfdd25e6aa9d490d860c5476ee0c3dab5838109ad6';
-}
